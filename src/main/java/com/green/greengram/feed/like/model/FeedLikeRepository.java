76145715cceb747577db19a6b48ab64469ada4f3
SongEunA <dmsdktt@gmail.com>
2025-03-12 09:54:32 +0900

    FeedLike test 세팅 중

---
+++ src/main/java/com/green/greengram/feed/like/model/FeedLikeRepository.java
@@ -0,0 +1,8 @@
+package com.green.greengram.feed.like.model;
+
+import com.green.greengram.entity.FeedLike;
+import com.green.greengram.entity.FeedLikeIds;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FeedLikeRepository extends JpaRepository<FeedLike, FeedLikeIds> {
+}
